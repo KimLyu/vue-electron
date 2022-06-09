a181bd862fffdd74f6b2acdb1dbbf23d22e78d4f
KimLyu <goustx@gmail.com>
2022-06-09 10:59:12 +0800

    使用nodejs可直接驅動硬體的code，放到網頁驅動會有問題，驅動部分暫時中止， 需要WebHID、WebUSB之類，要再研究研究。

--- src/services/eth_service.js
+++ src/services/eth_service.js
@@ -1,3 +1,38 @@
 export const ethSend = () => {
     console.log('ethSend');
+
+    var net = require('net-socket');
+    var socket = net.connect(9100, '192.168.1.151');
+    var echo_msg = '';
+
+    socket.setEncoding('hex');
+    socket.setTimeout(5000);
+    socket.on('timeout', () => {
+        // timeout 事件
+        console.log('ethernet connect timeout');
+        socket.end();
+        socket.destroy();
+    });
+    socket.on('connect', () => {
+        // connected
+        console.log('ethernet conndeted');
+        const sendCommand = '&GET,PI,E,d\r\n';
+        console.log('sendCommand = ', sendCommand);
+        socket.write(sendCommand);
+
+        // socket.end();
+        // socket.destroy();
+    });
+
+    socket.on('data', (data) => {
+        // console.log('data =', data);
+        echo_msg += data;
+        socket.end();
+    });
+
+    socket.on('end', function () {
+        console.log('echo_msg = ', echo_msg);
+        // socket.end();
+        socket.destroy();
+    });
 }
